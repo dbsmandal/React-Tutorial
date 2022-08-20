05d56fe993f516b714277c418e5b65af1ffa70be
dbsmandal <dipakmandaljio420@gmail.com>
2022-08-20 13:37:35 +0530

    add some note for better understanding

--- react-tutorial/src/State/UseStateClass.js
+++ react-tutorial/src/State/UseStateClass.js
@@ -8,7 +8,7 @@ class  UseStateClass extends Component {
             data2:"Try to use React State in Class Component with update type is  Contineusly update",
             data3:0,
             data4:"Try to use React State in Class Component with update type is  number Decrease",
-            data5:0,
+            
 
         }
     }
@@ -29,7 +29,7 @@ class  UseStateClass extends Component {
         return (
             <div>
             
-              <h1>{this.state.data1}</h1>
+              <h1> this is h1 tag and here import data1 is ---{this.state.data1}</h1>
               <button onClick={()=>this.JsxElement()}> Update Data</button>
               <button onClick={()=>this.Contineusly()}> Update Data jsx type Contineusly</button>
 
@@ -44,3 +44,60 @@ class  UseStateClass extends Component {
 
 }
 export default UseStateClass;
+
+
+//When ever use State in class Component Follow some Steps like Below
+//1. create class component like that 
+// import React, { Component } from 'react'
+// class  UseStateClass extends Component {    
+    
+//     render(){
+//         return (
+//             <div>
+            
+    
+//             </div>
+//           )   
+//     }
+
+// }
+// export default UseStateClass;
+
+//2. Define a constructor 
+//3.Add data as you want like that 
+
+// constructor()
+// {
+//     super();
+//     this.state={
+//         data1:"Try to use React State in Class Component with update type is Jsx Element like Content",
+//         data2:"Try to use React State in Class Component with update type is  Contineusly update",
+//         data3:0,
+//         data4:"Try to use React State in Class Component with update type is  number Decrease",
+        
+
+//     }
+// }
+
+//4.Whenever try to update state then require a button  and define that data wich you ant update help of state thats why create a button add a arrow function inside  button tag like that (line a.line b)
+
+{/*line a. <h1> this is h1 tag and here import data1 is ---{this.state.data1}</h1> */}
+{/* line b. <button onClick={()=>this.JsxElement()}> Update Data</button> */}
+
+
+//5.Define that function wich are allready declare in your button like that (JsxElement)
+// JsxElement(){
+//     this.setState({data1:"Update this data with help of state in class component"})
+// }
+//note that : If you udate one time then you can use above syntax but if you want try to update continuesly then use this type of syntax in function 
+// Contineusly(){
+//     this.setState({data1:this.state.data1+"Update Contineusly"})
+// }
+//And if you want update contineusly (Incrase or Decrase  button type)  then you can use this type of synatax
+
+// decraseNumber(){
+//     this.setState({data3:this.state.data3-1})
+// }
+// increaseNumber(){
+//     this.setState({data3:this.state.data3+1})
+// }
