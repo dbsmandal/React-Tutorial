25fdc69938ce4b5aecc4fc6952d4f5b11e8da763
dbsmandal <dipakmandaljio420@gmail.com>
2022-08-23 11:15:18 +0530

    try to use useEfect contionally

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,34 +1,11 @@
 import './App.css';
-import ClickEvent from './ClickEvent10/ClickEvent';
-import FunProps from './Props/FunProps';
-import StateInFunCompo from './State/StateInFunCompo';
-import UseStateClass from './State/UseStateClass';
-import UseStateFun from './State/UseStateFun';
-import FunPropsBtnClick from './Props/FunPropsBtnClick'
-import ClassProps from './Props/ClassProps';
+import UseEfectWithCondition from './Use Efect/UseEfectWithCondition';
+
 function App() {
   return (
-  <><div>
-    <ClickEvent/>
-    </div>
-  
-  <br />
-  <div className='function-state'> <StateInFunCompo/>
-  <br />
-  <UseStateFun/>
-  <StateInFunCompo/>
-  </div>
- <div className="classCompo-State">
-<UseStateClass/>
- </div>
- <div className="props">
-  <FunProps/>
- </div>
- <div className="FunProps-mulple-data">
-  <FunPropsBtnClick/>
- </div>
- <div className="classProps">
-  <ClassProps/>
+  <>
+  <div>
+    <UseEfectWithCondition/>
  </div>
 
   
---
+++ react-tutorial/src/Use Efect/UseEfectWithCondition.js
@@ -0,0 +1,25 @@
+import React,{useEffect,useState} from 'react'
+
+
+export default function UseEfectWithCondition() {
+    const[data,setData]=useState(10);
+    const[count,setCount]=useState(0);
+    useEffect(()=>{
+console.warn("data is called")
+    },[data])
+    useEffect(()=>{
+        console.warn("count is called")
+            },[count])
+  return (
+    <div>
+<h1>Use Efect with Condition </h1>
+<h2>Data is : {data}</h2>
+<h2>Count is :{count}</h2>
+
+<button onClick={()=>setData(data+1)}>Update Data</button>
+<button onClick={()=>setCount(count+1)}>Update Count</button>
+
+
+    </div>
+  )
+}
